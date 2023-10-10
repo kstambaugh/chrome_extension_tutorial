5cfbc706c8d17df91cc92d2824bff503915f10e4
Kyle <kyle.w.stambaugh1@gmail.com>
2023-10-10 12:55:23 -0400

    finished the tutorial

---
+++ exhange.js
@@ -0,0 +1,33 @@
+const amount = document.getElementById('amount');
+const currency = document.getElementById('currency');
+const convert = document.getElementById('convert');
+const result = document.getElementById('result');
+
+//api key obtained from api-ninja.com
+const apiKey = ""
+
+
+const apiURL=`https://api.api-ninjas.com/v1/exchangerate?pair=USD_`
+
+
+convert.addEventListener('click', () =>{
+    const amountTotal = amount.value;
+    const currencyTotal = currency.value;
+    const url = apiURL + currencyTotal;
+    
+    fetch(url, {
+        headers: {
+            'X-API-KEY': apiKey
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        const rate = data.exchange_rate;
+        const resultPrice = amountTotal * rate;
+        result.innerHTML = `${amountTotal} ${currencyTotal} = ${resultPrice.toFixed(2)} USD`;
+    })
+    .catch(error => {
+        console.error('Request failed:', error);
+        result.innerHTML = 'An error as occured, please try again later.'
+    })
+} )
